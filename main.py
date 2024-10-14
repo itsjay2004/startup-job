edf1a526ec46e9f543b65cfe6f174998ed8268c3
jaybauri0 <jaybauri0@gmail.com>
2024-10-14 15:05:29 +0000

    Initializing project

---
+++ main.py
@@ -0,0 +1,12 @@
+# Suggested code may be subject to a license. Learn more: ~LicenseLog:3824528574.
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
