6829d7af32b605173f4a5750a9828d4e45a197de
jaybauri0 <jaybauri0@gmail.com>
2024-10-15 08:44:16 +0000

    shifted back to bootstrap

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html', jobs=JOBS)
 
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
